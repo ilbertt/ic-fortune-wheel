a01aafd03f886f2d8fb445531b9a55b9c1133d60
ilbertt <luca.berte98@hotmail.it>
2025-03-29 14:47:51 +0100

    refactor: remove old frontend

--- src/frontend-old/src/app/(unauthenticated)/layout.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { printVersionToConsole } from '@/lib/utils';
-
-printVersionToConsole();
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
--- src/frontend-old/src/lib/wheel-prize-extraction.ts
+++
@@ -1,7 +0,0 @@
-import type { WheelPrizeExtractionState } from '@/declarations/backend/backend.did';
-
-export const isWheelPrizeExtractionCompleted = (
-  state: WheelPrizeExtractionState,
-): state is Extract<WheelPrizeExtractionState, { completed: unknown }> => {
-  return 'completed' in state;
-};
